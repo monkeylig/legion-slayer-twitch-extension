6ad3a4aa5c6565a6a148408569fab81ebeffb4d4
Justin Hardin <jhard9000@gmail.com>
2024-01-07 16:24:32 -0800

    sandboxed new text component

--- src/pages/sandbox.js
+++ src/pages/sandbox.js
@@ -7,6 +7,7 @@ import { useRef, useState } from "react";
 import useTypeWriterAnimation from "@/utilities/animation/useTypeWriterAnimation";
 import useNumberAnimation from "@/utilities/animation/useNumberAnimation";
 import useIntAnimation from "@/utilities/animation/useIntAnimation";
+import colors from "@/utilities/colors";
 
 export default function Sandbox() {
     
@@ -19,7 +20,7 @@ export default function Sandbox() {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <HeaderBar title='Sandbox'/>
-            <TypeWriterTest/>
+            <TextTest/>
         </>
     );
 }
@@ -80,5 +81,68 @@ function IntAnimTest() {
                 {num}
             </div>
         </>
+    );
+}
+
+function TextTest() {
+    const styleData = [
+        {
+            text: `k\\w*r`,
+            style: ({children})=> <span style={{color: 'green'}}>{children}</span>
+        },
+        {
+            text: 'Text',
+            style: ({children})=> <span style={{color: 'red'}}>{children}</span>
+        },
+        {
+            text: '\\d+% physical protection',
+            style: ({children})=> <span style={{color: colors.blue}}>{children}</span>
+        },
+        {
+            text: '\\d+% magical protection',
+            style: ({children})=> <span style={{color: colors.orange}}>{children}</span>
+        },
+        {
+            text: 'containing',
+            style: ({children})=> <span style={{color: 'pink'}}>{children}</span>
+        },
+        {
+            text: 'that need s',
+            style: ({children})=> <span style={{color: 'gold'}}>{children}</span>
+        },
+    ];
+    return <Text styleData={styleData}>Text containing keywords that need special formating 70% physical protection 30% magical protection</Text>;
+}
+
+function Text({styleData = [], children}) {
+    
+    const masterExp = "(" + styleData.reduce((accumulator, currentValue, index) => {
+        if (index === 0) {
+            return accumulator;
+        }
+        
+        return `${accumulator}|${currentValue.text}`;
+    }, styleData[0] ? styleData[0].text : '') + ")";
+
+    const searchExp = new RegExp(masterExp);
+    const splitText = children.split(searchExp).filter(chunk => chunk !== '');
+
+    const styledText = splitText.map((chunk, index) => {
+        for (const textStyle of styleData) {
+            let textMatch = new RegExp(textStyle.text);
+            if (textMatch.test(chunk)) {
+                const StyleFunc = textStyle.style;
+                return <StyleFunc key={index}>{chunk}</StyleFunc>
+            }
+        }
+
+        return chunk;
+    });
+
+    console.log(masterExp);
+    return (
+        <span>
+            {styledText}
+        </span>
     );
 }
